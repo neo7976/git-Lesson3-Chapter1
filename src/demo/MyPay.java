af62964c74e66bb8555e2a74a14c4566596dc920
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-08 10:00:39 +0300

    Paement page

---
+++ src/demo/MyPay.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class MyPay {
+
+    private int money;
+
+    public MyPay(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
